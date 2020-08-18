c58695eca5a230652ac20bbf6d5062fd7a5014b0
Mark Wales <mark@developme.tech>
2020-08-18 15:43:05 +0100

    challenges: adds additional lifecycle challenges

---
+++ challenges/04-lifecycle-methods/answers/tricksy/LightBox.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+
+const lightBoxStyle = {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    background: "rgba(0, 0, 0, 0.8)",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 1,
+};
+
+class LightBox extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lightBox: false,
+        };
+
+        this.hide = this.hide.bind(this);
+        this.show = this.show.bind(this);
+    }
+
+    // listen on the window for any clicks
+    componentDidMount() {
+        window.addEventListener("click", this.hide);
+    }
+
+    // remove window event listener when
+    // component is removed
+    componentWillUnmount() {
+        window.removeEventListener("click", this.hide);
+    }
+
+    hide() {
+        this.setState({ lightBox: false });
+    }
+
+    show(e) {
+        // don't want to trigger window event
+        // will immediately set back to false
+        e.stopPropagation();
+        this.setState({ lightBox: true });
+    }
+
+    render() {
+        const { lightBox } = this.state;
+        const { src } = this.props;
+
+        return (
+            <div style={ lightBox ? lightBoxStyle : {} }>
+                <img
+                    className="img-thumbnail"
+                    onClick={ this.show }
+                    style={ {
+                        width: lightBox ? "" : 50,
+                        margin: "0 auto",
+                        zIndex: 2,
+                    } }
+                    src={ src }
+                />
+            </div>
+        );
+    }
+}
+
+export default LightBox;
