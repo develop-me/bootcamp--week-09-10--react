2c0556a4a0acbbe80a96884b629827bb3be2895b
Flo Orford <flo.orford@hotmail.com>
2020-08-25 09:48:04 +0200

    cat gif links no longer work, updated with other cat gifs

--- scripts/02-props/resources/Figure.js
+++ scripts/02-props/resources/Figure.js
@@ -4,13 +4,11 @@ const Figure = () => (
     <figure className="card">
         <img
             className="card-img-top"
-            alt="A cat, strutting its stuff!"
-            src="https://goo.gl/tRdW93"
+            alt="A crazy cat!"
+            src="https://giphy.com/gifs/dz1iM8gU3RhzQy2MC7/html5"
         />
         <div className="card-body">
-            <figcaption className="card-text">
-                A cat, strutting its stuff!
-            </figcaption>
+            <figcaption className="card-text">A crazy cat!</figcaption>
         </div>
     </figure>
 );
