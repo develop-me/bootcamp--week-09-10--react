925f4a86bb9a3c70442ead7b8971c9db58d2d58e
Mark Wales <mark@developme.tech>
2020-12-01 16:12:41 +0000

    challenges: remove improperly placed comment
    wasn't that funny anyway

--- challenges/10-hooks/02-useReducer/ToDo/List.js
+++ challenges/10-hooks/02-useReducer/ToDo/List.js
@@ -45,7 +45,6 @@ const List = () => {
         { items.length === 0 ? <p className="card-text">No list items</p> : (
           <ul className="list-group list-group-flush">
             { items.map((item, index) => (
-              { /* so many bootstrap classes! */ }
               <li
                 key={ index }
                 className="list-group-item d-flex justify-content-between align-items-center"
